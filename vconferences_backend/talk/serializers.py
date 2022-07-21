c906d7cb7d34c64043831bbfa7a867e90ac7116c
Ayano <s.correa.wilches@gmail.com>
2022-07-21 08:39:52 +0700

    Edited serializer to bring in speaker info

--- vconferences_backend/talk/serializers.py
+++ vconferences_backend/talk/serializers.py
@@ -1,8 +1,15 @@
+from speaker.serializers import SpeakerSerializer
 from talk.models import Talk
 from rest_framework import serializers
 
 
 class TalkSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['speaker'] = SpeakerSerializer(instance.speaker).data
+        return data
+
     class Meta:
         model = Talk
         fields = ('id', 'title', 'abstract',
