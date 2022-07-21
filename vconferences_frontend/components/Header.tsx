a31f0b4aaff90b1457d209a6f6bd974ee6b84759
Ayano <s.correa.wilches@gmail.com>
2022-07-21 08:23:19 +0700

    Added navbar navigation & stylings

--- vconferences_frontend/components/Header.tsx
+++ vconferences_frontend/components/Header.tsx
@@ -4,12 +4,34 @@ import Link from "next/link";
 
 import styles from "../styles/Home.module.scss";
 
+type Row = {
+  title: string;
+  href: string;
+};
+
+const rows: Row[] = [
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Conferences",
+    href: "/conferences",
+  },
+];
+
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <div className={styles.navbar}>
+        {rows.map((row: Row, index: number) => (
+          <div key={index} className={styles.navitem}>
+            <Link href={row.href}>
+              <a>{row.title}</a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
